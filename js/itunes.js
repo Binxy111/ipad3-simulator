07f6c78f1c60f8d15ec2e26db7b0b8554308b291
Sean Coker <seany789@gmail.com>
2012-11-02 16:18:55 -0400

    itunes library model

---
+++ js/itunes.js
@@ -0,0 +1,32 @@
+var Itunes = {
+
+	BASE_DIR: 'itunes',
+
+	library: [{
+		dir: 'P64 - First Take- There is No Reason to be Upset',
+		album: 'P64 - First Take- There is No Reason to be Upset',
+		songs: [
+			'P64 - First Take- There is No Reason to be Upset - 01 We the People (Intro) (Prod. Dan Be).mp3',
+			'P64 - First Take- There is No Reason to be Upset - 02 Smooth.mp3',
+			'P64 - First Take- There is No Reason to be Upset - 03 Bitch (HYFR Remix).mp3',
+			'P64 - First Take- There is No Reason to be Upset - 04 Three Seasons.mp3',
+			'P64 - First Take- There is No Reason to be Upset - 05 Sowwy (prod. by J Dilla).mp3',
+			'P64 - First Take- There is No Reason to be Upset - 06 Illest Villains on the Block (ft. I.R.V.).mp3',
+			'P64 - First Take- There is No Reason to be Upset - 07 Touch the Crown (6\'7 Remix).mp3',
+			'P64 - First Take- There is No Reason to be Upset - 08 Get Up In My Section (prod. by Whatnik).mp3',
+			'P64 - First Take- There is No Reason to be Upset - 09 Loud Pipes (prod. by Ratatat).mp3',
+			'P64 - First Take- There is No Reason to be Upset - 10 Swaggin\' Ball Z (ft. O.H.M.).mp3',
+			'P64 - First Take- There is No Reason to be Upset - 11 She Will & Rigormortis Reprise.mp3',
+			'P64 - First Take- There is No Reason to be Upset - 12 Keyroll (prod. Nazzy).mp3',
+			'P64 - First Take- There is No Reason to be Upset - 13 Luck\'s Up (prod. 9th Wonder).mp3',
+			'P64 - First Take- There is No Reason to be Upset - 14 Last Page of the Yearbook (prod. by 9th Wonder).mp3',
+			'P64 - First Take- There is No Reason to be Upset - 15 Miss Me.mp3',
+			'P64 - First Take- There is No Reason to be Upset - 16 Death Wish (Last Kiss).mp3',
+			'P64 - First Take- There is No Reason to be Upset - 17 Pete\'s Out (Outro).mp3',
+			'P64 - First Take- There is No Reason to be Upset - 18 Gucci Gucci -BONUS- (prod. by DJ 2 Stacks).mp3',
+			'P64 - First Take- There is No Reason to be Upset - 19 Full Gauge -BONUS-.mp3',
+			'P64 - First Take- There is No Reason to be Upset - 20 Budda\' Flow (-BONUS-.mp3'
+		],
+		cover: 'cover.jpg'
+	}]
+};
